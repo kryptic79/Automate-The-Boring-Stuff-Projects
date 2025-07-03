90fe0361e035f4fd732b558323ce3903d0c4ab8c
Krypt <49000170+kryptic79@users.noreply.github.com>
2025-07-02 20:55:38 -0400

    Create Chapter01.py

---
+++ Chapter01.py
@@ -0,0 +1,28 @@
+#Automate the boring stuff pratice projects
+#Phillip Oiwoh
+#Chapter 1
+
+def collatz(number):
+    if number % 2 == 0:
+        number = number // 2
+        print(number)
+        return(number)
+    elif number % 2 == 1:
+        number = 3 * number + 1 
+        print(number)
+        return number
+
+try:
+    print("Enter number:")
+    number = input()
+    while(number!=1):
+        number = collatz(int(number))
+        
+except ValueError:
+    print("Please enter interger value")
+    
+
+
+
+
+
